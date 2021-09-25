63a53c35f38880aa6dfe7ef721c648f48a70949a
EkaterinaO <66118096+EkaterinaED@users.noreply.github.com>
2021-09-25 19:39:26 +0300

    added iOS part

---
+++ Eco Route/Eco Route/Eco Route/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    private let webView = WKWebView(frame: .zero)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.webView)
+        NSLayoutConstraint.activate([
+            self.webView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.webView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.webView.topAnchor.constraint(equalTo: self.view.topAnchor),
+        ])
+        self.view.setNeedsLayout()
+        if let url = URL(string: "https://www.google.com") {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
+    }
+}
+
