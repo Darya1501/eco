f6af557f2d95c2547db7215bc9a68f2fa1569d0f
Darya <darya_01@mail.ru>
2021-09-25 10:40:45 +0300

    1

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,3 +1,17 @@
 map = new OpenLayers.Map("demoMap");
 map.addLayer(new OpenLayers.Layer.OSM());
-map.zoomToMaxExtent();
+
+const lonLat = new OpenLayers.LonLat( 37.61556, 55.75222)
+  .transform(
+    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+    map.getProjectionObject() // to Spherical Mercator Projection
+  );
+
+const zoom=12;
+
+const markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+
+markers.addMarker(new OpenLayers.Marker(lonLat));
+
+map.setCenter (lonLat, zoom);
