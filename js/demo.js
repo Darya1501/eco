32dca4a88f35a619cfeee088ad4f4b39e7bab212
guzuro <guzurogmn@gmail.com>
2021-09-26 04:08:59 +0300

    add trash icons toggler on map

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,364 @@
+// AirPolutionData = [
+//   {
+//     StreetName: "Улица 1905 года",
+//     CO2: "473.00",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.764625",
+//     longitude: "37.561237",
+//   },
+//   {
+//     StreetName: "Баррикадная",
+//     CO2: "473.00",
+//     LOS: "9.69",
+//     AQI: "94.4",
+//     latitude: "55.760861",
+//     longitude: "37.579600",
+//   },
+//   {
+//     StreetName: "Бережковская набережная",
+//     CO2: "614.00",
+//     LOS: "44,359.00",
+//     AQI: "122.7",
+//     latitude: "55.735452",
+//     longitude: "37.561650",
+//   },
+//   {
+//     StreetName: "Варшавское шоссе",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.608765",
+//     longitude: "37.605135",
+//   },
+//   {
+//     StreetName: "Волгоградский проспект",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.707670",
+//     longitude: "37.757134",
+//   },
+//   {
+//     StreetName: "Воробьёвская набережная",
+//     CO2: "614.00",
+//     LOS: "44,359.00",
+//     AQI: "122.00",
+//     latitude: "55.718212",
+//     longitude: "37.539534",
+//   },
+//   {
+//     StreetName: "Госпитальная набережная",
+//     CO2: "473.00",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.771836",
+//     longitude: "37.694903",
+//   },
+//   {
+//     StreetName: "Дмитровское шоссе",
+//     CO2: "331.00",
+//     LOS: "44,414.00",
+//     AQI: "122.00",
+//     latitude: "55.915071",
+//     longitude: "37.544651",
+//   },
+//   {
+//     StreetName: "Звенигородское шоссе",
+//     CO2: "331.00",
+//     LOS: "44,414.00",
+//     AQI: "66.00",
+//     latitude: "55.767327",
+//     longitude: "37.528394",
+//   },
+//   {
+//     StreetName: "Каширское шоссе",
+//     CO2: "236.00",
+//     LOS: "44,412.00",
+//     AQI: "47.2",
+//     latitude: "55.531524",
+//     longitude: "37.767493",
+//   },
+//   {
+//     StreetName: "Кетчерская улица",
+//     CO2: "236.00",
+//     LOS: "44,412.00",
+//     AQI: "47.2",
+//     latitude: "55.743867",
+//     longitude: "37.826907",
+//   },
+//   {
+//     StreetName: "Королева улица",
+//     CO2: "331.00",
+//     LOS: "44,414.00",
+//     AQI: "66.00",
+//     latitude: "55.821618",
+//     longitude: "37.611870",
+//   },
+//   {
+//     StreetName: "Крымская набережная",
+//     CO2: "473.00",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.736479",
+//     longitude: "37.605611",
+//   },
+//   {
+//     StreetName: "Энтузиастов шоссе",
+//     CO2: "236.5",
+//     LOS: "44,412.00",
+//     AQI: "47.1",
+//     latitude: "55.778822",
+//     longitude: "37.851412",
+//   },
+//   {
+//     StreetName: "Щелковское шоссе",
+//     CO2: "236.7",
+//     LOS: "44,412.00",
+//     AQI: "47.1",
+//     latitude: "55.897670",
+//     longitude: "38.037790",
+//   },
+//   {
+//     StreetName: "Цветной бульвар",
+//     CO2: "473.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.770414",
+//     longitude: "37.622249",
+//   },
+//   {
+//     StreetName: "Тверская улица",
+//     CO2: "331.3",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.764760",
+//     longitude: "37.605509",
+//   },
+//   {
+//     StreetName: "Таганская улица",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.740014",
+//     longitude: "37.662942",
+//   },
+//   {
+//     StreetName: "Страстной бульвар",
+//     CO2: "473.1",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.766678",
+//     longitude: "37.609204",
+//   },
+//   {
+//     StreetName: "Сретенский бульвар",
+//     CO2: "331.1",
+//     LOS: "44,414.00",
+//     AQI: "66.00",
+//     latitude: "55.766299",
+//     longitude: "37.633745",
+//   },
+//   {
+//     StreetName: "Солженицына улица",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.743653",
+//     longitude: "37.661707",
+//   },
+//   {
+//     StreetName: "Садовая-самотечная улица",
+//     CO2: "331.1",
+//     LOS: "44,414.00",
+//     AQI: "61.1",
+//     latitude: "55.773784",
+//     longitude: "37.617485",
+//   },
+//   {
+//     StreetName: "Садовая-каретная улица",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.772978",
+//     longitude: "37.608251",
+//   },
+//   {
+//     StreetName: "Рождественский бульвар",
+//     CO2: "473.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.766610",
+//     longitude: "37.627506",
+//   },
+//   {
+//     StreetName: "Пятницкая улица",
+//     CO2: "473.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.738198",
+//     longitude: "37.628206",
+//   },
+//   {
+//     StreetName: "Профсоюзная улица",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.00",
+//     latitude: "55.649522",
+//     longitude: "37.532416",
+//   },
+//   {
+//     StreetName: "Проспект Вернадского",
+//     CO2: "472.9",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.682355",
+//     longitude: "37.516987",
+//   },
+//   {
+//     StreetName: "Петровский бульвар",
+//     CO2: "373.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.767596",
+//     longitude: "37.617631",
+//   },
+//   {
+//     StreetName: "Новый Арбат",
+//     CO2: "331.3",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.752642",
+//     longitude: "37.587667",
+//   },
+//   {
+//     StreetName: "Новодевичья набережная",
+//     CO2: "615.1",
+//     LOS: "44,359.00",
+//     AQI: "122.7",
+//     latitude: "55.728217",
+//     longitude: "37.551892",
+//   },
+//   {
+//     StreetName: "Никитская Б. улица",
+//     CO2: "473.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.757532",
+//     longitude: "37.598229",
+//   },
+//   {
+//     StreetName: "Нагатскинская набережная",
+//     CO2: "473.2",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.685073",
+//     longitude: "37.661319",
+//   },
+//   {
+//     StreetName: "Набережная Тараса Шевченко",
+//     CO2: "473.1",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.748448",
+//     longitude: "37.550148",
+//   },
+//   {
+//     StreetName: "Моховая улица",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.752753",
+//     longitude: "37.611202",
+//   },
+//   {
+//     StreetName: "Мосфильмосвкая улица",
+//     CO2: "614.9",
+//     LOS: "44,359.00",
+//     AQI: "122.7",
+//     latitude: "55.714779",
+//     longitude: "37.513997",
+//   },
+//   {
+//     StreetName: "Минское шоссе",
+//     CO2: "615.2",
+//     LOS: "44,359.00",
+//     AQI: "122.7",
+//     latitude: "55.567148",
+//     longitude: "36.659155",
+//   },
+//   {
+//     StreetName: "Люблинская улица",
+//     CO2: "236.5",
+//     LOS: "44,412.00",
+//     AQI: "47.1",
+//     latitude: "55.673160",
+//     longitude: "37.739492",
+//   },
+//   {
+//     StreetName: "Лужнецкая набережная",
+//     CO2: "473.1",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "55.711466",
+//     longitude: "37.557084",
+//   },
+//   {
+//     StreetName: "Ленинградское шоссе",
+//     CO2: "473.00",
+//     LOS: "44,386.00",
+//     AQI: "94.4",
+//     latitude: "56.222924",
+//     longitude: "36.920751",
+//   },
+//   {
+//     StreetName: "Кутузовский проспект",
+//     CO2: "331.2",
+//     LOS: "44,414.00",
+//     AQI: "66.1",
+//     latitude: "55.735441",
+//     longitude: "37.511535",
+//   },
+// ];
+
+TrashMarkersData = [
+  { latitude: "55.764625", longitude: "37.561237" },
+
+  { latitude: "55.707670", longitude: "37.757134" },
+  {
+    latitude: "55.915071",
+    longitude: "37.544651",
+  },
+  {
+    latitude: "55.531524",
+    longitude: "37.767493",
+  },
+  {
+    latitude: "55.736479",
+    longitude: "37.605611",
+  },
+  {
+    latitude: "55.770414",
+    longitude: "37.622249",
+  },
+  {
+    latitude: "55.766678",
+    longitude: "37.609204",
+  },
+  {
+    latitude: "55.773784",
+    longitude: "37.617485",
+  },
+  {
+    latitude: "55.649522",
+    longitude: "37.532416",
+  },
+  {
+    latitude: "55.685073",
+    longitude: "37.661319",
+  },
+];
+
 var iconObject = L.icon({
   iconUrl: "./img/marker-icon.png",
   shadowSize: [50, 64],
@@ -5,8 +366,21 @@ var iconObject = L.icon({
   iconAnchor: [12, 40],
 });
 
+var trashIcon = L.icon({
+  iconUrl: "./img/trashMarker.png",
+  shadowSize: [50, 64],
+  shadowAnchor: [4, 62],
+  iconAnchor: [12, 40],
+});
+
 var currentProfile = "foot";
 
+var marker = L.icon({
+  iconUrl: "./img/marker.png",
+  shadowSize: [50, 64],
+  shadowAnchor: [4, 62],
+  iconAnchor: [12, 40],
+});
 $(document).ready(function (e) {
   // reqButton.addEventListener("click", clickCallback);
 
@@ -37,15 +411,35 @@ $(document).ready(function (e) {
   var defaultKey = "c9b2b65d-be3d-4d96-b19c-48365a066ae3";
   var profile = "car";
 
-
-  transportBtn = document.querySelectorAll('.sidebar-btn');
-  transportBtn.forEach(btn => {
-    btn.addEventListener('click', event => {
-      currentProfile = event.target.closest('.sidebar-btn').id;
+  transportBtn = document.querySelectorAll(".sidebar-btn");
+  transportBtn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      currentProfile = event.target.closest(".sidebar-btn").id;
       let myEvent = new Event("click");
-      const reqButton = document.getElementById("reqButton").dispatchEvent(myEvent);
+      const reqButton = document
+        .getElementById("reqButton")
+        .dispatchEvent(myEvent);
+    });
+  });
+  let trashIcons = null
+  setTimeout(() => {
+    trashIcons = document.querySelectorAll(".leaflet-marker-icon");
+  }, 1000);
+
+
+  document.getElementById('trash-can').addEventListener("change", function(e){
+    trashIcons.forEach(icon=>{
+      e.target.checked ? icon.style.display = 'block' : icon.style.display = 'none'
     })
   })
+
+  // transportBtn.forEach(btn => {
+  //   btn.addEventListener('click', event => {
+  //     currentProfile = event.target.closest('.sidebar-btn').id;
+  //     let myEvent = new Event("click");
+  //     const reqButton = document.getElementById("reqButton").dispatchEvent(myEvent);
+  //   })
+  // })
 
   // create a routing client to fetch real routes, elevation.true is only supported for vehicle bike or foot
   var ghRouting = new GraphHopper.Routing({
@@ -69,17 +463,25 @@ $(document).ready(function (e) {
 
 function setupRoutingAPI(map, ghRouting) {
   map.setView([55.76, 37.57], 13);
-
   ghRouting.vehicle = currentProfile;
+
+  // "latitude": "55.736479",
+  // "longitude": "37.605611"
 
   var instructionsDiv = $("#instructions");
   map.on("click", function (e) {
     if (ghRouting.points.length > 1) {
       ghRouting.clearPoints();
       routingLayer.clearLayers();
+      // AirPolutionData.forEach((m) => {
+      //   L.marker([m.latitude, m.longitude], { icon: iconObject }).addTo(
+      //     routingLayer
+      //   );
+      // });
     }
 
     L.marker(e.latlng, { icon: iconObject }).addTo(routingLayer);
+
     ghRouting.addPoint(new GHInput(e.latlng.lat, e.latlng.lng));
     if (ghRouting.points.length > 1) {
       // ******************
@@ -160,7 +562,14 @@ function setupRoutingAPI(map, ghRouting) {
     ghRouting.clearPoints();
     routingLayer.clearLayers();
     ghRouting.vehicle = currentProfile;
-    console.log(map, routingLayer);
+    if (ghRouting.points.length > 1) {
+      // AirPolutionData.forEach((m) => {
+      //   L.marker([m.latitude, m.longitude], { icon: iconObject }).addTo(
+      //     routingLayer
+      //   );
+      // });
+    }
+
     const from = document.getElementById("from");
     const to = document.getElementById("to");
     const API_KEY = "c9KsMyw2ruxx9v-I4LIYmyZ9r1gu4twXCyeF6w4sZT0";
@@ -206,79 +615,107 @@ function setupRoutingAPI(map, ghRouting) {
             });
 
             markers.forEach((r) => {
-                L.marker(r, { icon: iconObject }).addTo(routingLayer);
-                ghRouting.addPoint(new GHInput(r.lat, r.lng));
-              });
-            
-              // ******************
-              //  Calculate route!
-              // ******************
-              ghRouting
-                .doRequest()
-                .then(function (json) {
-                  var path = json.paths[0];
-                  routingLayer.addData({
-                    type: "Feature",
-                    geometry: path.points,
-                  });
-                  var outHtml = "Distance in meter:" + path.distance;
-                  outHtml += "<br/>Times in seconds:" + path.time / 1000;
-                  outHtml +=
-                    "<br/><a href='" +
-                    ghRouting.getGraphHopperMapsLink() +
-                    "'>GraphHopper Maps</a>";
-                  $("#routing-response").html(outHtml);
-            
-                  if (path.bbox) {
-                    var minLon = path.bbox[0];
-                    var minLat = path.bbox[1];
-                    var maxLon = path.bbox[2];
-                    var maxLat = path.bbox[3];
-                    var tmpB = new L.LatLngBounds(
-                      new L.LatLng(minLat, minLon),
-                      new L.LatLng(maxLat, maxLon)
-                    );
-                    map.fitBounds(tmpB);
-                  }
-            
-                  instructionsDiv.empty();
-                  if (path.instructions) {
-                    var allPoints = path.points.coordinates;
-                    var listUL = $("<ol>");
-                    instructionsDiv.append(listUL);
-                    for (var idx in path.instructions) {
-                      var instr = path.instructions[idx];
-            
-                      // use 'interval' to find the geometry (list of points) until the next instruction
-                      var instruction_points = allPoints.slice(
-                        instr.interval[0],
-                        instr.interval[1]
-                      );
-            
-                      // use 'sign' to display e.g. equally named images
-            
-                      $(
-                        "<li>" +
-                          instr.text +
-                          " <small>(" +
-                          ghRouting.getTurnText(instr.sign) +
-                          ")</small>" +
-                          " for " +
-                          instr.distance +
-                          "m and " +
-                          Math.round(instr.time / 1000) +
-                          "sec" +
-                          ", geometry points:" +
-                          instruction_points.length +
-                          "</li>"
-                      ).appendTo(listUL);
-                    }
-                  }
-                })
-                .catch(function (err) {
-                  var str = "An error occured: " + err.message;
-                  $("#routing-response").text(str);
+              L.marker(r, { icon: iconObject }).addTo(routingLayer);
+              ghRouting.addPoint(new GHInput(r.lat, r.lng));
+            });
+
+            /**/
+            // let latitute = markers[0].lat;
+            // let longitude = markers[0].lng;
+
+            // let latDiff = AirPolutionData.slice().map((l) => {
+            //   return Math.sqrt(
+            //     Math.pow(Number(latitute) - Number(l.latitude), 2) +
+            //       Math.pow(Number(longitude) - Number(l.longitude), 2)
+            //   );
+            // });
+
+            // var indexMenor = latDiff.indexOf(Math.min(...latDiff));
+
+            // console.log(indexMenor);
+
+            // const nearMarker = AirPolutionData[28];
+
+            // console.log(latDiff);
+            // routingLayer.options.style.color = "red";
+            /**/
+
+            // ******************
+            //  Calculate route!
+            // ******************
+            ghRouting
+              .doRequest()
+              .then(function (json) {
+                var path = json.paths[0];
+                console.log(path);
+                routingLayer.addData({
+                  type: "Feature",
+                  geometry: path.points,
                 });
+                // var pathCopy = path;
+                // pathCopy.points.coordinates[7][0] = 40.1111;
+                // routingLayer.addData({
+                //   type: "Feature",
+                //   geometry: pathCopy.points,
+                // });
+                var outHtml = "Distance in meter:" + path.distance;
+                outHtml += "<br/>Times in seconds:" + path.time / 1000;
+                outHtml +=
+                  "<br/><a href='" +
+                  ghRouting.getGraphHopperMapsLink() +
+                  "'>GraphHopper Maps</a>";
+                $("#routing-response").html(outHtml);
+
+                if (path.bbox) {
+                  var minLon = path.bbox[0];
+                  var minLat = path.bbox[1];
+                  var maxLon = path.bbox[2];
+                  var maxLat = path.bbox[3];
+                  var tmpB = new L.LatLngBounds(
+                    new L.LatLng(minLat, minLon),
+                    new L.LatLng(maxLat, maxLon)
+                  );
+                  map.fitBounds(tmpB);
+                }
+
+                instructionsDiv.empty();
+                if (path.instructions) {
+                  var allPoints = path.points.coordinates;
+                  var listUL = $("<ol>");
+                  instructionsDiv.append(listUL);
+                  for (var idx in path.instructions) {
+                    var instr = path.instructions[idx];
+
+                    // use 'interval' to find the geometry (list of points) until the next instruction
+                    var instruction_points = allPoints.slice(
+                      instr.interval[0],
+                      instr.interval[1]
+                    );
+
+                    // use 'sign' to display e.g. equally named images
+
+                    $(
+                      "<li>" +
+                        instr.text +
+                        " <small>(" +
+                        ghRouting.getTurnText(instr.sign) +
+                        ")</small>" +
+                        " for " +
+                        instr.distance +
+                        "m and " +
+                        Math.round(instr.time / 1000) +
+                        "sec" +
+                        ", geometry points:" +
+                        instruction_points.length +
+                        "</li>"
+                    ).appendTo(listUL);
+                  }
+                }
+              })
+              .catch(function (err) {
+                var str = "An error occured: " + err.message;
+                $("#routing-response").text(str);
+              });
           },
         });
       },
@@ -294,11 +731,12 @@ function setupRoutingAPI(map, ghRouting) {
   routingLayer.options = {
     style: { color: "#00cc33", weight: 5, opacity: 0.6 },
   };
-}
 
-function makeRoute(map, routingLayer) {
-
-  
+  TrashMarkersData.forEach((t) => {
+    L.marker([t.latitude, t.longitude], { icon: trashIcon }).addTo(
+      routingLayer
+    );
+  });
 }
 
 function createMap(divId) {
